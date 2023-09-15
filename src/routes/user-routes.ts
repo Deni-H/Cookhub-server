e4425866ad27707eaedde55fea0865b76eb935b1
Deni-H <denyhilhamsyh@gmail.com>
2023-09-16 03:40:37 +0700

    Moving followUser on user-routes from /user/:userId/follow/ to /user/follow/:userId/
    Moving unfollowUser on user-routes
    from /user/:userId/unfollow/ to /user/unfollow/:userId/

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -3,26 +3,26 @@ import * as UserController from "../controllers/user-controller"
 
 export const userRouter = express.Router()
 
-userRouter.get("/user", UserController.getUserDetails)
+userRouter.get("/user/", UserController.getUserDetails)
 
 userRouter.get("/user/:userId", UserController.getUserProfile)
 
-userRouter.post("/user", UserController.addUserProfile)
+userRouter.post("/user/", UserController.addUserProfile)
 
-userRouter.put("/user", UserController.updateUserProfile)
+userRouter.put("/user/", UserController.updateUserProfile)
 
-userRouter.post("/user/username", UserController.setUserName)
+userRouter.post("/user/username/", UserController.setUserName)
 
-userRouter.post("/user/:userId/follow", UserController.followUser)
+userRouter.post("/user/follow/:userId/", UserController.followUser)
 
-userRouter.post("/user/:userId/unfollow", UserController.unfollowUser)
+userRouter.post("/user/unfollow/:userId/", UserController.unfollowUser)
 
-userRouter.get("/user/following/:userId", UserController.isFollowing)
+userRouter.get("/user/following/:userId/", UserController.isFollowing)
 
-userRouter.get("/user/:userId/followers", UserController.getFollowers)
+userRouter.get("/user/:userId/followers/", UserController.getFollowers)
 
-userRouter.get("/user/:userId/following", UserController.getFollowing)
+userRouter.get("/user/:userId/following/", UserController.getFollowing)
 
-userRouter.get("/user/:userId/recipes", UserController.getRecipes)
+userRouter.get("/user/:userId/recipes/", UserController.getRecipes)
 
-userRouter.post("/username", UserController.isUserNameExists)
+userRouter.post("/username/", UserController.isUserNameExists)
